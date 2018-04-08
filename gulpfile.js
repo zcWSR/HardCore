2367a427dc6ad8a9d2d8aa18542c19a393959098
congzz.zhao <congzz.zhao@qunar.com>
2018-04-08 12:10:48 +0800

    Feat: 添加gulp watch, 更换markdown样式

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp =  require('gulp');
 var gulp_sass  = require('gulp-sass'); 
+var gulp_concat = require('gulp-concat');
 
 gulp.task('build', () => {
+    // return gulp.src(['./HardCore.scss', './markdown.css'])
     return gulp.src('./HardCore.scss')
     .pipe(gulp_sass().on('error',gulp_sass.logError))
+    // .pipe(gulp_concat('HardCore.css'))
     .pipe(gulp.dest('./build'));
 });
+
+gulp.task('watch', () => {
+    return gulp.watch('./**/*.scss', ['build']);
+})
